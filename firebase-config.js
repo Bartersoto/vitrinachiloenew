3185f7067d9a9dac13c26055d796f9e7013cfae4
Bartersoto <sotobarter44@gmail.com>
2025-07-12 21:55:33 -0400

    s

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,19 @@
-// firebase-config.js
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuración Firebase clásica (v8) compatible con GitHub Pages
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDMcOSCXXx02REkqWjqAJ9LIxGXmNgE2DA",
+    authDomain: "vitrina-chiloe-nueva.firebaseapp.com",
+    projectId: "vitrina-chiloe-nueva",
+    storageBucket: "vitrina-chiloe-nueva.firebasestorage.app",
+    messagingSenderId: "16256233627",
+    appId: "1:16256233627:web:ca7a0fa9cd552cbade563e"
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDMcOSCXXx02REkqWjqAJ9LIxGXmNgE2DA",
-  authDomain: "vitrina-chiloe-nueva.firebaseapp.com",
-  projectId: "vitrina-chiloe-nueva",
-  storageBucket: "vitrina-chiloe-nueva.appspot.com",
-  messagingSenderId: "16256233627",
-  appId: "1:16256233627:web:ca7a0fa9cd552cbade563e"
-};
-
-// Inicializa Firebase (v8.x)
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>

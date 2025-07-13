ef492e880b01821cadcaea26ae4697e399fa8602
Bartersoto <sotobarter44@gmail.com>
2025-07-12 21:45:38 -0400

    s

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
-// Configuración Firebase clásica (para usar con GitHub Pages)
+// firebase-config.js
 
 const firebaseConfig = {
   apiKey: "AIzaSyDMcOSCXXx02REkqWjqAJ9LIxGXmNgE2DA",
   authDomain: "vitrina-chiloe-nueva.firebaseapp.com",
   projectId: "vitrina-chiloe-nueva",
-  storageBucket: "vitrina-chiloe-nueva.appspot.com",
+  storageBucket: "vitrina-chiloe-nueva.firebasestorage.app",
   messagingSenderId: "16256233627",
   appId: "1:16256233627:web:ca7a0fa9cd552cbade563e"
 };
 
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const storage = firebase.storage();

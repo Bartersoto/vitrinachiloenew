1f206761f7fc7fbf191f7082c1cf7be685f45371
Bartersoto <sotobarter44@gmail.com>
2025-07-12 21:41:21 -0400

    A

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,15 @@
-// Inicializa Firebase con tu config
+// Configuración Firebase clásica (para usar con GitHub Pages)
+
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDMcOSCXXx02REkqWjqAJ9LIxGXmNgE2DA",
+  authDomain: "vitrina-chiloe-nueva.firebaseapp.com",
+  projectId: "vitrina-chiloe-nueva",
+  storageBucket: "vitrina-chiloe-nueva.appspot.com",
+  messagingSenderId: "16256233627",
+  appId: "1:16256233627:web:ca7a0fa9cd552cbade563e"
 };
 
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
